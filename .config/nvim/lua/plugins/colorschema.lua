9f0a9574dc4f5b33a984b9a821d2dbb65b2675c3
Daniel Rueffer <daniel.rueffer@parcelly.com>
2023-10-08 17:34:41 +0200

    use onedark for neovim

--- .config/nvim/lua/plugins/colorschema.lua
+++ .config/nvim/lua/plugins/colorschema.lua
@@ -3,45 +3,28 @@ return {
   -- { "rmehri01/onenord.nvim" },
 
   -- add onedark
-  -- { "navarasu/onedark.nvim" },
+  {
+    "navarasu/onedark.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = { style = "warmer" },
+  },
 
-  -- add catppuccino
-  { "catppuccin/nvim" },
+  -- add catppuccin
+  -- { "catppuccin/nvim" },
 
-  -- Configure LazyVim to load colorscheme
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = true,
+  --   priority = 1000,
+  --   opts = { style = "storm" },
+  -- },
+
+  -- Configure LazyVim to load onedark
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
-      integrations = {
-        alpha = true,
-        cmp = true,
-        flash = true,
-        gitsigns = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        lsp_trouble = true,
-        mason = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        noice = true,
-        notify = true,
-        neotree = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        which_key = true,
-      },
+      colorscheme = "onedark",
     },
   },
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   opts = {
     autotag = { enable = true },
     endwise = { enable = true },
-    highlight = { enable = true, additional_vim_regex_highlighting = true },
+    highlight = { enable = true, additional_vim_regex_highlighting = false },
     indent = { enable = true },
     context_commentstring = { enable = true, enable_autocmd = false },
     ensure_installed = {
