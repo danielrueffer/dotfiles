a407626b1fcf1dfacf472427ed66a2c217ea8fe6
Daniel Rueffer <daniel.rueffer@parcelly.com>
2024-02-14 22:58:56 +0100

    new dashboard with random quotes

--- .config/nvim-custom/lua/plugins/alpha.lua
+++ .config/nvim-custom/lua/plugins/alpha.lua
@@ -1,72 +1,174 @@
-return {
-  "goolord/alpha-nvim",
-  event = "VimEnter", -- load plugin after all configuration is set
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
-
-    dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-    }
-
-    _Gopts = {
-      position = "center",
-      hl = "Type",
-      -- wrap = "overflow";
-    }
-
-    -- Set menu
-    dashboard.section.buttons.val = {
-      -- dashboard.button("SPC j", "󰈚   Restore Session", ":SessionRestore<cr>"),
-      dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "   Find file", ":cd $HOME/dotfiles | Telescope find_files<CR>"),
-      dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
-      dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
-      dashboard.button("c", "   Config", ":e $MYVIMRC <CR>"),
-      dashboard.button("m", "󱌣   Mason", ":Mason<CR>"),
-      dashboard.button("l", "󰒲   Lazy", ":Lazy<CR>"),
-      dashboard.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
-      dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
-    }
-
-    dashboard.opts.opts.noautocmd = true
-    alpha.setup(dashboard.opts)
-
-    require('alpha').setup(dashboard.opts)
-
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'LazyVimStarted',
-      callback = function()
-        local stats = require('lazy').stats()
-        local count = (math.floor(stats.startuptime * 100) / 100)
-        dashboard.section.footer.val = {
-          "󱐌 " .. stats.count .. " plugins loaded in " .. count .. " ms",
-          " ",
-          "      Make Cool Stuff"
-        }
-        pcall(vim.cmd.AlphaRedraw)
-      end,
-    })
-  end,
+local coolLines = {
+  [[    ███╗   ███╗ █████╗ ██╗  ██╗███████╗   ]],
+  [[    ████╗ ████║██╔══██╗██║ ██╔╝██╔════╝   ]],
+  [[    ██╔████╔██║███████║█████╔╝ █████╗     ]],
+  [[    ██║╚██╔╝██║██╔══██║██╔═██╗ ██╔══╝     ]],
+  [[    ██║ ╚═╝ ██║██║  ██║██║  ██╗███████╗   ]],
+  [[    ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝   ]],
+  [[      ██████╗ ██████╗  ██████╗ ██╗        ]],
+  [[     ██╔════╝██╔═══██╗██╔═══██╗██║        ]],
+  [[     ██║     ██║   ██║██║   ██║██║        ]],
+  [[     ██║     ██║   ██║██║   ██║██║        ]],
+  [[     ╚██████╗╚██████╔╝╚██████╔╝███████╗   ]],
+  [[      ╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝   ]],
+  [[███████╗████████╗██╗   ██╗███████╗███████╗]],
+  [[██╔════╝╚══██╔══╝██║   ██║██╔════╝██╔════╝]],
+  [[███████╗   ██║   ██║   ██║█████╗  █████╗  ]],
+  [[╚════██║   ██║   ██║   ██║██╔══╝  ██╔══╝  ]],
+  [[███████║   ██║   ╚██████╔╝██║     ██║     ]],
+  [[╚══════╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝     ]],
 }
+
+local solveLines = {
+  [[╔═╗┬┬─┐┌─┐┌┬┐  ┌─┐┌─┐┬ ┬  ┬┌─┐  ┌┬┐┬ ┬┌─┐  ┌─┐┬─┐┌─┐┌┐ ┬  ┌─┐┌┬┐   ]],
+  [[╠╣ │├┬┘└─┐ │   └─┐│ ││ └┐┌┘├┤    │ ├─┤├┤   ├─┘├┬┘│ │├┴┐│  ├┤ │││   ]],
+  [[╚  ┴┴└─└─┘ ┴┘  └─┘└─┘┴─┘└┘ └─┘   ┴ ┴ ┴└─┘  ┴  ┴└─└─┘└─┘┴─┘└─┘┴ ┴ o ]],
+  [[      ╔╦╗┬ ┬┌─┐┌┐┌  ┬ ┬┬─┐┬┌┬┐┌─┐  ┌┬┐┬ ┬┌─┐  ┌─┐┌─┐┌┬┐┌─┐         ]],
+  [[       ║ ├─┤├┤ │││  │││├┬┘│ │ ├┤    │ ├─┤├┤   │  │ │ ││├┤          ]],
+  [[       ╩ ┴ ┴└─┘┘└┘  └┴┘┴└─┴ ┴ └─┘   ┴ ┴ ┴└─┘  └─┘└─┘─┴┘└─┘         ]],
+  [[                                                     - John Johnson]]
+}
+
+local humourLines = {
+  [[╔═╗┌─┐┌┬┐┌─┐  ┬┌─┐  ┬  ┬┬┌─┌─┐  ┬ ┬┬ ┬┌┬┐┌─┐┬─┐                           ]],
+  [[║  │ │ ││├┤   │└─┐  │  │├┴┐├┤   ├─┤│ │││││ │├┬┘                           ]],
+  [[╚═╝└─┘─┴┘└─┘  ┴└─┘  ┴─┘┴┴ ┴└─┘  ┴ ┴└─┘┴ ┴└─┘┴└─o                          ]],
+  [[╦ ╦┬ ┬┌─┐┌┐┌  ┬ ┬┌─┐┬ ┬  ┬ ┬┌─┐┬  ┬┌─┐  ┌┬┐┌─┐  ┌─┐─┐ ┬┌─┐┬  ┌─┐┬┌┐┌  ┬┌┬┐]],
+  [[║║║├─┤├┤ │││  └┬┘│ ││ │  ├─┤├─┤└┐┌┘├┤    │ │ │  ├┤ ┌┴┬┘├─┘│  ├─┤││││  │ │ ]],
+  [[╚╩╝┴ ┴└─┘┘└┘   ┴ └─┘└─┘  ┴ ┴┴ ┴ └┘ └─┘   ┴ └─┘  └─┘┴ └─┴  ┴─┘┴ ┴┴┘└┘  ┴ ┴┘]],
+  [[                                                        ┬┌┬┐┌─┐  ┌┐ ┌─┐┌┬┐]],
+  [[                                                        │ │ └─┐  ├┴┐├─┤ ││]],
+  [[                                                        ┴ ┴ └─┘  └─┘┴ ┴─┴┘]],
+  [[                                                              - Cory House]],
+}
+
+local bugLines = {
+  [[  _____   _                          _                 _                             ]],
+  [[ |_   _| | |_    ___   _ _   ___    (_)  ___    __ _  | | __ __ __  __ _   _  _   ___]],
+  [[   | |   | ' \  / -_) | '_| / -_)   | | (_-<   / _` | | | \ V  V / / _` | | || | (_-<]],
+  [[   |_|   |_||_| \___| |_|   \___|   |_| /__/   \__,_| |_|  \_/\_/  \__,_|  \_, | /__/]],
+  [[                                                                           |__/      ]],
+  [[                                                   _                                 ]],
+  [[  ___   _ _    ___     _ __    ___   _ _   ___    | |__   _  _   __ _                ]],
+  [[ / _ \ | ' \  / -_)   | '  \  / _ \ | '_| / -_)   | '_ \ | || | / _` |               ]],
+  [[ \___/ |_||_| \___|   |_|_|_| \___/ |_|   \___|   |_.__/  \_,_| \__, |               ]],
+  [[                                                                |___/                ]],
+  [[  _              __   _                                                              ]],
+  [[ | |_   ___     / _| (_) __ __                                                       ]],
+  [[ |  _| / _ \   |  _| | | \ \ /  _                                                    ]],
+  [[  \__| \___/   |_|   |_| /_\_\ (_)                                     - Ellen Ullman]],
+}
+
+local fixLines = {
+  [[    ┌─┐┬─┐ ┬  ┌┬┐┬ ┬┌─┐  ┌─┐┌─┐┬ ┬┌─┐┌─┐      ]],
+  [[    ├┤ │┌┴┬┘   │ ├─┤├┤   │  ├─┤│ │└─┐├┤       ]],
+  [[    └  ┴┴ └─   ┴ ┴ ┴└─┘  └─┘┴ ┴└─┘└─┘└─┘┘     ]],
+  [[╔╗╔╔═╗╔╦╗  ╔╦╗╦ ╦╔═╗  ╔═╗╦ ╦╔╦╗╔═╗╔╦╗╔═╗╔╦╗╔═╗]],
+  [[║║║║ ║ ║    ║ ╠═╣║╣   ╚═╗╚╦╝║║║╠═╝ ║ ║ ║║║║╚═╗]],
+  [[╝╚╝╚═╝ ╩    ╩ ╩ ╩╚═╝  ╚═╝ ╩ ╩ ╩╩   ╩ ╚═╝╩ ╩╚═╝]],
+  [[                              - Steve Maguire ]],
+}
+
+local processLines = {
+  [[╔╦╗╦ ╦╔═╗  ╔═╗╦═╗╔═╗╔═╗╔╦╗╦╦  ╦╔═╗  ╔═╗╦═╗╔═╗╔═╗╔═╗╔═╗╔═╗]],
+  [[ ║ ╠═╣║╣   ║  ╠╦╝║╣ ╠═╣ ║ ║╚╗╔╝║╣   ╠═╝╠╦╝║ ║║  ║╣ ╚═╗╚═╗]],
+  [[ ╩ ╩ ╩╚═╝  ╚═╝╩╚═╚═╝╩ ╩ ╩ ╩ ╚╝ ╚═╝  ╩  ╩╚═╚═╝╚═╝╚═╝╚═╝╚═╝]],
+  [[This is Amazing!]],
+  [[This is difficult]],
+  [[This is shit]],
+  [[I am shit]],
+  [[This might be OK]],
+  [[This is Amazing!]],
+}
+
+local function lineColor(lines, popStart, popEnd)
+  local out = {}
+  for i, line in ipairs(lines) do
+    local hi = "StartLogo" .. i
+    if i > popStart and i <= popEnd then
+      hi = "StartLogoPop" .. i - popStart
+    elseif i > popStart then
+      hi = "StartLogo" .. i - popStart
+    else
+      hi = "StartLogo" .. i
+    end
+    table.insert(out, { hi = hi, line = line})
+  end
+  return out
+end
+
+local headers = {
+  lineColor(coolLines, 6, 12),
+  lineColor(solveLines, 0, 0),
+  lineColor(humourLines, 6, 9),
+  lineColor(bugLines, 5, 10),
+  lineColor(processLines, 0, 3),
+  lineColor(fixLines, 0, 0),
+}
+
+local function header_chars()
+  math.randomseed(os.time())
+  return headers[math.random(#headers)]
+end
+
+-- Map over the headers, setting a different color for each line.
+-- This is done by setting the Highligh to StartLogoN, where N is the row index.
+-- Define StartLogo1..StartLogoN to get a nice gradient.
+local function header_color()
+  local lines = {}
+  for _, lineConfig in pairs(header_chars()) do
+    local hi = lineConfig.hi
+    local line_chars = lineConfig.line
+    local line = {
+      type = "text",
+      val = line_chars,
+      opts = {
+        hl = hi,
+        shrink_margin = false,
+        position = "center",
+      },
+    }
+    table.insert(lines, line)
+  end
+
+  local output = {
+    type = "group",
+    val = lines,
+    opts = { position = "center", },
+  }
+
+  return output
+end
+
+local function configure()
+  local theme = require("alpha.themes.theta")
+  local themeconfig = theme.config
+  local dashboard = require("alpha.themes.dashboard")
+  local buttons = {
+    type = "group",
+    val = {
+      { type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+      { type = "padding", val = 1 },
+      dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+      dashboard.button("SPC f", "  Find file"),
+      dashboard.button("SPC F", "  Find text"),
+      dashboard.button("u", "󱐥  Update plugins", "<cmd>Lazy sync<CR>"),
+      dashboard.button("t", "  Install language tools", "<cmd>Mason<CR>"),
+      dashboard.button("q", "󰩈  Quit", "<cmd>qa<CR>"),
+    },
+    position = "center",
+  }
+
+  themeconfig.layout[2] = header_color()
+  themeconfig.layout[6] = buttons
+
+  return themeconfig
+end
+
+return {
+  'goolord/alpha-nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function ()
+    require'alpha'.setup(configure())
+  end
+};
