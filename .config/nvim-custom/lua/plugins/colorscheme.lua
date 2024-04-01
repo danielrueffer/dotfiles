bef66424c3ddb8c4595656255d4ba2ce15c19862
Daniel Rueffer <daniel.rueffer@parcelly.com>
2024-04-01 11:59:21 +0200

    add catppuccin theme

--- .config/nvim-custom/lua/plugins/colorscheme.lua
+++ .config/nvim-custom/lua/plugins/colorscheme.lua
@@ -1,36 +1,12 @@
 return {
-	"rebelot/kanagawa.nvim",
-	name = "kanagawa",
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
 	lazy = false,
 	config = function()
-		require("kanagawa").setup({
-			colors = {
-				theme = {
-					all = {
-						ui = { bg_gutter = "none" },
-					},
-				},
-			},
-			overrides = function(colors)
-				local theme = colors.theme
-				return {
-					NormalFloat = { bg = "none" },
-					FloatBorder = { bg = "none" },
-					FloatTitle = { bg = "none" },
-
-					-- Save an hlgroup with dark background and dimmed foreground
-					-- so that you can use it where your still want darker windows.
-					-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-					-- Popular plugins that open floats will link to NormalFloat by default;
-					-- set their background accordingly if you wish to keep them dark and borderless
-					LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-				}
-			end,
+		require("catppuccin").setup({
+			flavour = "mocha",
 		})
-		vim.cmd.colorscheme("kanagawa")
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
--- .config/nvim-custom/lua/plugins/lualine.lua
+++ .config/nvim-custom/lua/plugins/lualine.lua
@@ -1,10 +1,10 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require("lualine").setup()
-    options = {
-      theme = "kanagawa"
-    }
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup()
+		options = {
+			theme = "catppucin",
+		}
+	end,
 }
