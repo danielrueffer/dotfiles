e0bb2c6d995dba22bf74a0079c5a4d1a4368c6d8
Daniel Rueffer <daniel.rueffer@parcelly.com>
2024-04-21 19:41:51 +0200

    basic lualine config

--- .config/nvim-custom/lua/plugins/lualine.lua
+++ .config/nvim-custom/lua/plugins/lualine.lua
@@ -2,9 +2,6 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("lualine").setup()
-		options = {
-			theme = "catppucin",
-		}
+		require("lualine").setup({})
 	end,
 }
