35ca6fed85265173a88fd863c61317baca9c9ab7
Daniel Rüffer <daniel.rueffer@icloud.com>
2024-07-02 21:22:40 +0200

    disable default provider

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -7,6 +7,12 @@ opt.shell = "/bin/bash"
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 -- line numbers
 opt.relativenumber = true
 opt.number = true
