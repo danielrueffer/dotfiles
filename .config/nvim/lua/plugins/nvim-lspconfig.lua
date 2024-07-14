fd7b64365c1ebbe36f80fd95cf7080c7d74bf2ba
Daniel Rüffer <daniel.rueffer@icloud.com>
2024-07-14 17:54:56 +0200

    feat: change diagnostic icons

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -80,6 +80,14 @@ return {
 			end,
 		})
 
+		-- Change the Diagnostic symbols in the sign column (gutter)
+		-- (not in youtube nvim video)
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
+
 		-- Configure borderd for LspInfo ui
 		require("lspconfig.ui.windows").default_options.border = "rounded"
 	end,
