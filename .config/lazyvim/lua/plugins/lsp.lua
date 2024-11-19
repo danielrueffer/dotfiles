d3829957ea692db0914aaa74774c18b9e4ab402b
Daniel Rüffer <daniel.rueffer@icloud.com>
2024-11-19 22:41:15 +0100

    feat: setup ruby-lsp

---
+++ .config/lazyvim/lua/plugins/lsp.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ruby_lsp = {
+          mason = false,
+          enabled = true,
+          cmd = { "/Users/danielrueffer/.asdf/shims/ruby-lsp" },
+          filetypes = { "ruby", "rakefile" },
+        },
+      },
+    },
+  },
+}
