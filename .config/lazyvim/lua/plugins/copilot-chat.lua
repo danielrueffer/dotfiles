7d7fe021b7af0b10dd0a8aceacc618cea8e17118
Daniel Rüffer <daniel.rueffer@icloud.com>
2025-03-10 09:22:37 +0100

    feat: add specs prompt

---
+++ .config/lazyvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,10 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  opts = {
+    prompts = {
+      Specs = {
+        prompt = "> /COPILOT_GENERATE\n\nPlease generate specs for my code.",
+      },
+    },
+  },
+}
