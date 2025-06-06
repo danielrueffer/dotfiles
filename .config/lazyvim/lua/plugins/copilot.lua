77a3b97596d48783777fd9e7b2427bfb002c59af
Daniel Rüffer <daniel.rueffer@icloud.com>
2025-06-06 11:57:00 +0200

    feat: change copilot default model

--- .config/lazyvim/lua/plugins/copilot.lua
+++ .config/lazyvim/lua/plugins/copilot.lua
@@ -18,41 +18,8 @@ return {
       },
     },
     lazy = false, -- Load this plugin immediately (adjust based on your preference)
-    branch = "create-pull-request/update-copilot-dist",
-
-    config = function(_, opts)
-      local new_opts = vim.tbl_extend("force", opts, {
-        server_opts_overrides = {
-          cmd = {
-            "node",
-            vim.api.nvim_get_runtime_file("copilot/dist/language-server.js", false)[1],
-            "--stdio",
-          },
-          init_options = {
-            copilotIntegrationId = "vscode-chat",
-          },
-        },
-      })
-
-      require("copilot").setup(new_opts)
-
-      local util = require("copilot.util")
-      local orig_get_editor_configuration = util.get_editor_configuration
-
-      ---@diagnostic disable-next-line: duplicate-set-field
-      util.get_editor_configuration = function()
-        local config = orig_get_editor_configuration()
-
-        return vim.tbl_extend("force", config, {
-          github = {
-            copilot = {
-              selectedCompletionModel = "gpt-4o-copilot",
-            },
-          },
-        })
-      end
-
-      return new_opts
-    end,
+    options = {
+      copilot_model = "gpt-4o-copilot",
+    },
   },
 }
