e597a79b85d9e81c078719a34efa937856b229c9
Daniel Rüffer <daniel.rueffer@icloud.com>
2025-10-17 10:46:39 +0200

    feat: use bin/run instead of bin/test

--- .config/lazyvim/lua/plugins/neotest.lua
+++ .config/lazyvim/lua/plugins/neotest.lua
@@ -7,14 +7,32 @@ return {
   opts = {
     adapters = {
       ["neotest-rspec"] = {
-        rspec_cmd = function()
-          return vim.tbl_flatten({
-            "bin/test",
-          })
+        rspec_cmd = function(position_type)
+          if position_type == "test" then
+            return vim
+              .iter({
+                "bin/run",
+                "rspec",
+                "--fail-fast",
+              })
+              :flatten()
+              :totable()
+          else
+            return vim
+              .iter({
+                "bin/run",
+                "rspec",
+              })
+              :flatten()
+              :totable()
+          end
         end,
 
         transform_spec_path = function(path)
           local prefix = require("neotest-rspec").root(path)
+          if not prefix then
+            return path
+          end
           return string.sub(path, string.len(prefix) + 2, -1)
         end,
 
