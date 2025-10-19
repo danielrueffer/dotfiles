5b88a790b36886a5f7433a9c3fa352bc324f2c6e
Daniel Rüffer <daniel.rueffer@icloud.com>
2025-10-19 22:32:44 +0200

    feat: enable copilot NES

--- .config/lazyvim/lua/config/options.lua
+++ .config/lazyvim/lua/config/options.lua
@@ -8,8 +8,4 @@ vim.g.loaded_python_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
--- LSP Server to use for Ruby.
--- Set to "solargraph" to use solargraph instead of ruby_lsp.
-vim.g.lazyvim_ruby_lsp = "ruby_lsp"
-vim.g.lazyvim_ruby_formatter = "rubocop"
--- vim.lsp.enable("copilot")
+vim.lsp.enable("copilot")
