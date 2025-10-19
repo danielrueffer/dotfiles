eea8330fe152f8fe5beff048a1c4ec45f0757aad
Daniel Rüffer <daniel.rueffer@icloud.com>
2025-10-19 22:32:27 +0200

    feat: add keymaps

--- .config/lazyvim/lua/config/keymaps.lua
+++ .config/lazyvim/lua/config/keymaps.lua
@@ -2,6 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Increment/decrement
+vim.keymap.set("n", "+", "<C-a>")
+vim.keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+vim.keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
 vim.keymap.set(
   "n",
   "<leader>gg",
